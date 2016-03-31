df3950a210e7dd11c97147e6a7445a7c7bee4c35
marcustran87 <marcus@highwire.io>
2016-03-31 23:02:28 +0700

    update

---
+++ newstyle/js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+    
+     /*$('li').click(function(){
+        if( ! $( this ).hasClass( 'active' ) ){
+            $(this).parent(".menu").find("li.active").removeClass("active");
+            $(this).addClass("active");
+        }
+         else
+             {
+                 return false;
+             }
+        
+    });
+    
+    */
+    
+    // Generate
+    var hoiio="";
+   
+    hoiio += "    <div class=\"logo\">";
+    hoiio += "        <img src=\"img\/square.svg\">";
+    hoiio += "        <img src=\"img\/tri.svg\">";
+    hoiio += "        <img src=\"img\/round.svg\">";
+    hoiio += "    <\/div>";
+    hoiio += "    <ul class=\"leftMenu menu\">";
+    hoiio += "        <li class=\"item\">";
+    hoiio += "            <a class=\"links\" href=\"about\">About<\/a>";
+    hoiio += "        <\/li>";
+    hoiio += "        <li class=\"item\">";
+    hoiio += "            <a class=\"links\" href=\"grid\">Structure<\/a>";
+    hoiio += "        <\/li>";
+    hoiio += "        <li class=\"item\">";
+    hoiio += "            <a class=\"links\" href=\"color\">Color<\/a>";
+    hoiio += "        <\/li>";
+    hoiio += "        <li class=\"item\">";
+    hoiio += "            <a class=\"links\" href=\"font\">Fonts<\/a>";
+    hoiio += "        <\/li>";
+    hoiio += "        <li class=\"item\">";
+    hoiio += "            <a class=\"links\" href=\"components.html\">Components<\/a>";
+    hoiio += "        <\/li>";
+    hoiio += "    <\/ul>";
+   
+    $(".navbar").append(hoiio);
+    
+    var x = window.location.href.match(/.*\/(.*)$/)[1];
+    
+    if(x){
+        $('a[href$='+x+']').addClass("active");
+    }
+    else{
+        $('a[href$=introduction').addClass("active");
+    }
+ 
+});
