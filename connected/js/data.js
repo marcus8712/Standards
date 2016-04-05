8006f7348269039df12dc4fd5ff2ce065af66da8
marcustran87 <marcus@highwire.io>
2016-04-05 14:18:34 +0700

    update

--- connected/js/data.js
+++ connected/js/data.js
@@ -1,6 +1,14 @@
-
-d3.csv("/xanhchi/data/xanhchi.csv", function(error, data) {
+d3.csv("/standards/connected/data/staff.csv", function(error, data) {
     data.forEach(function(d) {
-         d3.select("body").append("div").text(d.productID);
+        d3.select("table tbody tr").append("tr").attr("id",d.id);
+        
+        
     });
 });
+
+/*
+id avatar	first_name	last_name	phone	role_portal	subscriptons	last_access_portal
+d3.select("#"+ d.id).append("td").text(d.first_name + d.last_name);
+        d3.select("table tbody tr").append("td").text(d.phone);
+
+*/
