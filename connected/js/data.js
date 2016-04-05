20a89e9134cc3f009599b7f12c0be9203f4c344d
marcustran87 <marcus@highwire.io>
2016-04-05 15:30:16 +0700

    update

--- connected/js/data.js
+++ connected/js/data.js
@@ -3,14 +3,7 @@ d3.csv("/standards/connected/data/staff.csv", function(error, data) {
    
     
     data.forEach(function(d) {
-       /* var dataset = [d.avatar,d.phone,d.role_portal,d.subscriptions,d.last_access_portal];
-        d3.select("table tbody").append("tr").attr("id", 'staff' + d.id);
-        d3.select(("#staff" + d.id),function()
-            .append("td").text(d.first_name)
-            .enter()
-            .append("td").text(d.last_name);*/
-
-        var diventer = d3.select("table tbody tr")
+       var diventer = d3.select("table tbody tr")
         .data(d)
       .enter().append("div")
         .attr("id", function(e) { return e.first_name; });
@@ -26,7 +19,12 @@ d3.csv("/standards/connected/data/staff.csv", function(error, data) {
 });
 /*
 
-
+var dataset = [d.avatar,d.phone,d.role_portal,d.subscriptions,d.last_access_portal];
+        d3.select("table tbody").append("tr").attr("id", 'staff' + d.id);
+        d3.select(("#staff" + d.id),function()
+            .append("td").text(d.first_name)
+            .enter()
+            .append("td").text(d.last_name);
 id avatar	first_name	last_name	phone	role_portal	subscriptons	last_access_portal
 d3.select("#"+ d.id).append("td").text(d.first_name + d.last_name);
         d3.select("table tbody tr").append("td").text(d.phone);
