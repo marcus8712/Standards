cc90e9754746039769224ec1c5661961b812dd1c
marcustran87 <marcus@highwire.io>
2016-04-05 14:21:22 +0700

    update

--- connected/js/data.js
+++ connected/js/data.js
@@ -1,6 +1,6 @@
 d3.csv("/standards/connected/data/staff.csv", function(error, data) {
     data.forEach(function(d) {
-        d3.select("table tbody tr").append("tr").attr("id",d.id);
+        d3.select("table tbody").append("tr").attr("id", d.id);
         
         
     });
