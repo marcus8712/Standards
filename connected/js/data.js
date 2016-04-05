3ab07c954c62f347e1aac1c9efd322df4236d339
marcustran87 <marcus@highwire.io>
2016-04-05 14:47:16 +0700

    update

--- connected/js/data.js
+++ connected/js/data.js
@@ -1,20 +1,11 @@
 d3.csv("/standards/connected/data/staff.csv", function(error, data) {
     data.forEach(function(d) {
-        d3.select("table tbody").append("tr");
-        d3.select("table tbody").selectAll("tr").attr("id", d.id);
+        d3.select("table tbody").append("tr").attr("id", 'staff' + d.id);
+       
         
     });
 });
 /*
-pdata = [10,12,6,8,15];
- 
-selectDIV = d3.select("#example1");
-
-selectDIV.selectAll("p")
- .data(pdata)
- .enter()
- .append("p")
- .text(function(d){return d;});
 
 
 id avatar	first_name	last_name	phone	role_portal	subscriptons	last_access_portal
