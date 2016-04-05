591743eb10ef22ae5a3c479d72595038b0f01197
marcustran87 <marcus@highwire.io>
2016-04-05 15:27:43 +0700

    update

--- connected/js/data.js
+++ connected/js/data.js
@@ -1,12 +1,25 @@
 d3.csv("/standards/connected/data/staff.csv", function(error, data) {
+    
+   
+    
     data.forEach(function(d) {
-        var dataset = [d.avatar,d.phone,d.role_portal,d.subscriptions,d.last_access_portal];
+       /* var dataset = [d.avatar,d.phone,d.role_portal,d.subscriptions,d.last_access_portal];
         d3.select("table tbody").append("tr").attr("id", 'staff' + d.id);
-        d3.select("#staff" + d.id)
+        d3.select(("#staff" + d.id),function()
             .append("td").text(d.first_name)
             .enter()
-            .append("td").text(d.last_name);
-        
+            .append("td").text(d.last_name);*/
+
+        var diventer = d3.select("table tbody tr")
+        .data(d)
+      .enter().append("div")
+        .attr("id", function(e) { return e.first_name; });
+
+    diventer.append("p")
+        .text(function(e) { return e.last_name; });
+
+    diventer.append("img")
+        .attr("src", function(e) { return e.phone; });
         
         
     });
