1cad72336a9a78233bced8da7c5be2dd37bdf62a
marcustran87 <marcus@highwire.io>
2016-04-05 16:59:05 +0700

    update

--- connected/js/data.js
+++ connected/js/data.js
@@ -5,10 +5,10 @@ d3.csv("/standards/connected/data/staff.csv", function(error, data) {
     data.forEach(function(d) {
         var diventer = d3.select("table tbody").append("tr").attr("id", 'stafff' + d.id);
         
-        diventer.append("td").text(d.first_name + ' ' + d.lastname);
+        diventer.append("td").text(d.first_name + ' ' + d.last_name);
         diventer.append("td").text(d.phone);
         diventer.append("td").text(d.role_portal);
-        diventer.append("td").text(d.subscriptions);
+        diventer.append("td").text(d.subscriptons);
         diventer.append("td").text(d.last_access_portal);
 
  
