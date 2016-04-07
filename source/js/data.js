c321aeb781030aa770acc35a7e73fe20f80a48d4
marcustran87 <marcus@highwire.io>
2016-04-07 14:37:15 +0700

    update member page

--- source/js/data.js
+++ source/js/data.js
@@ -4,7 +4,8 @@ d3.csv("/standards/source/data/staff.csv", function(error, data) {
     data.forEach(function(d) {
         var diventer = d3.select("table tbody").append("tr").attr("id", 'stafff' + d.id);
         
-        diventer.append("td").text(d.first_name + ' ' + d.last_name);
+        diventer.append("img").attr('class', 'ui avatar image').attr('src','http://marcus8712.github.io/standards/source/img/square-image.jpg');
+        d3.select("table tbody tr td").append("span").text(d.first_name + ' ' + d.last_name);
         diventer.append("td").text(d.phone);
         diventer.append("td").text(d.role_portal);
         diventer.append("td").text(d.subscriptons);
@@ -13,7 +14,8 @@ d3.csv("/standards/source/data/staff.csv", function(error, data) {
     });
 })
 /*
-
+<img class="ui avatar image" src="../img/square-image.jpg">
+                                            <span>Username</span>
 var dataset = [d.avatar,d.phone,d.role_portal,d.subscriptions,d.last_access_portal];
         d3.select("table tbody").append("tr").attr("id", 'staff' + d.id);
         d3.select(("#staff" + d.id),function()
